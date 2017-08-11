4b96959e317db959bd6c75eee3b44f1328fc1bec
Hizuro <hizuro@gmx.net>
2017-08-11 19:43:22 +0200

    added packager tags for all localizations

--- localization.lua
+++ localization.lua
@@ -4,21 +4,41 @@ ns.L = setmetatable({}, { __index = function(t, k) local v=tostring(k) rawset(t,
 local L = ns.L;
 
 if LOCALE_deDE then
-	--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 end
 
---if LOCALE_esES or LOCALE_esMX then end
+if LOCALE_esES then
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
 
---if LOCALE_frFR then end
+if LOCALE_esMX then
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
 
---if LOCALE_itIT then end
+if LOCALE_frFR then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
 
---if LOCALE_koKR then end
+if LOCALE_itIT then
+--@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
 
---if LOCALE_ptBR then end
+if LOCALE_koKR then
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
 
---if LOCALE_ruRU then end
+if LOCALE_ptBR then
+--@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
 
---if LOCALE_zhCN then end
+if LOCALE_ruRU then
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
 
---if LOCALE_zhTW then end
+if LOCALE_zhCN then
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
+
+if LOCALE_zhTW then
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+end
