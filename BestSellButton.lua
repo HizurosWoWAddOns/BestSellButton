3b4d17d6d658dfb918ba664d1b137e4447815276
Hizuro <hizuro@gmx.net>
2022-06-12 17:31:46 +0200

    update pkgmeta, add credit page and remove deprecated functions

--- BestSellButton.lua
+++ BestSellButton.lua
@@ -21,34 +21,6 @@ local priceIconsNone = {
 };
 
 -- [ misc functions ] --
-do
-	local addon_short = "BSB";
-	local colors = {"82c5ff","00ff00","ff6060","44ffff","ffff00","ff8800","ff44ff","ffffff"};
-	local function colorize(...)
-		local t,c,a1 = {tostringall(...)},1,...;
-		if type(a1)=="boolean" then tremove(t,1); end
-		if a1~=false then
-			tinsert(t,1,"|cff82c5ff"..((a1==true and addon_short) or (a1=="||" and "||") or addon).."|r"..(a1~="||" and HEADER_COLON or ""));
-			c=2;
-		end
-		for i=c, #t do
-			if not t[i]:find("\124c") then
-				t[i],c = "|cff"..colors[c]..t[i].."|r", c<#colors and c+1 or 1;
-			end
-		end
-		return unpack(t);
-	end
-	function ns.print(...)
-		print(colorize(...));
-	end
-	function ns.debug(...)
-		ConsolePrint(date("|cff999999%X|r"),colorize(...));
-	end
-	function ns.debugPrint(...)
-		print(colorize("<debug>",...));
-	end
-end
-
 local function iconstr(path)
 	return "|T"..path..":16:16|t";
 end
@@ -195,7 +167,13 @@ local options = {
 					}
 				}
 			}
-		}
+		},
+
+		credits = {
+			type = "group", order = 200,
+			name = L["Credits"],
+			args = {}
+		},
 	}
 };
 
@@ -248,6 +226,8 @@ function BestSellButtonMixin:OnEvent(event,...)
 
 		LibStub("AceConfig-3.0"):RegisterOptionsTable(addon, options);
 		LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addon);
+
+		ns.AddCredits(options.args.credits.args);
 
 		if self.db.profile.showAddOnLoaded or IsShiftKeyDown() then
 			ns.print(L["AddOnLoaded"]);
